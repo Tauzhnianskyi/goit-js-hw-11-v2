e23124aeb9b69d74515e5ee682143421b2a12e3f
Tauzhnianskyi <d.a.tauzhnianskyi@gmail.com>
2022-11-05 21:13:12 +0200

    1

--- src/index.js
+++ src/index.js
@@ -10,54 +10,47 @@ const search = document.getElementById('search');
 const searchBtn = document.querySelector('.searchBtn');
 console.log(search.value);
 
-getMovies(API_URL);
-
-function getMovies(url) {
-  fetch(url)
-    .then(resp => resp.json())
-    .then(data => {
-      showMovies(data.results);
-    });
-}
-
 function showMovies(data) {
   main.innerHTML = '';
 
   data.forEach(movie => {
-    const { title, backdrop_path, vote_average } = movie; 
+    const {
+      title,
+      backdrop_path,
+      vote_average,
+      id,
+      overview,
+      vote_count,
+      popularity,
+      genre_ids,
+    } = movie;
     const movieEl = document.createElement('div');
     movieEl.classList.add('movie');
     movieEl.innerHTML = `
     <img src="${IMG_URL + backdrop_path}" alt="${title}" width="100px" />
         <div class="movie-info">
           <h3 class="movie-title">${title}</h3>
-          <span class="${getColor(vote_average)}">${vote_average}</span>
+          <span class="avarage">${vote_average}</span>
+          <span class="id-number">${id}</span>
+          <button type="button" class="btn-${id}">Add to wish</button>
           </div>
     
     `;
     main.appendChild(movieEl);
+
+    const Storage = {
+      img: `${IMG_URL + backdrop_path}`,
+      title: `${title}`,
+      id: `${id}`,
+      overview: `${overview}`,
+      vote_count: `${vote_count}`,
+      popularity: `${popularity}`,
+      genre_ids: `${genre_ids}`,
+    };
+
+    const wishBtn = document.querySelector(`.btn-${id}`);
+    wishBtn.addEventListener('click', function handleClick() {
+      window.localStorage.setItem(`movie#${id}`, JSON.stringify(Storage));
+    });
   });
 }
-
-function getColor(vote) {
-  if (vote >= 8) {
-    return 'green';
-  } else if (vote >= 5 && vote < 8) {
-    return 'orange';
-  } else {
-    return 'red';
-  }
-}
-
-form.addEventListener('submit', e => {
-  e.preventDefault();
-
-  const searchTerm = search.value;
-  console.log(searchTerm);
-
-  if (searchTerm) {
-    getMovies(searchURL + '&query=' + searchTerm);
-  } else {
-    getMovies(API_URL);
-  }
-});
---
+++ src/js/api.js
@@ -0,0 +1,60 @@
+const BASE_URL = 'https://api.themoviedb.org/3';
+const API_KEY = 'api_key=14f00e78a1ac618254d40a79e60e9f37';
+const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&' + API_KEY;
+const IMG_URL = 'https://image.tmdb.org/t/p/w500';
+const searchURL = BASE_URL + '/search/keyword?' + API_KEY;
+
+async function getMovies(url) {
+  await fetch(url)
+    .then(resp => resp.json())
+    .then(data => {
+      showMovies(data.results);
+    });
+}
+
+function showMovies(data) {
+  main.innerHTML = '';
+
+  data.forEach(movie => {
+    const {
+      title,
+      backdrop_path,
+      vote_average,
+      id,
+      overview,
+      vote_count,
+      popularity,
+      genre_ids,
+    } = movie;
+    const movieEl = document.createElement('div');
+    movieEl.classList.add('movie');
+    movieEl.innerHTML = `
+    <img src="${IMG_URL + backdrop_path}" alt="${title}" width="100px" />
+        <div class="movie-info">
+          <h3 class="movie-title">${title}</h3>
+          <span class="avarage">${vote_average}</span>
+          <span class="id-number">${id}</span>
+          <button type="button" class="btn-${id}">Add to wish</button>
+          </div>
+    
+    `;
+    main.appendChild(movieEl);
+
+    const Storage = {
+      img: `${IMG_URL + backdrop_path}`,
+      title: `${title}`,
+      id: `${id}`,
+      overview: `${overview}`,
+      vote_count: `${vote_count}`,
+      popularity: `${popularity}`,
+      genre_ids: `${genre_ids}`,
+    };
+
+    const wishBtn = document.querySelector(`.btn-${id}`);
+    wishBtn.addEventListener('click', function handleClick() {
+      window.localStorage.setItem(`movie#${id}`, JSON.stringify(Storage));
+    });
+  });
+}
+
+console.log(getMovies(API_URL));
