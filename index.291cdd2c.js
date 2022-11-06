0160443596f97c70151133a280f5e763882c326d
Tauzhnianskyi <104843369+Tauzhnianskyi@users.noreply.github.com>
2022-11-06 14:31:36 +0000

    Deploying to gh-pages from @ Tauzhnianskyi/goit-js-hw-11-v2@4fdb2c3730087e9b427da4b8b5f4a17af54a386a 🚀

--- index.291cdd2c.js
+++
@@ -1,2 +0,0 @@
-document.getElementById("main"),document.querySelector("form");const e=document.getElementById("search");document.querySelector(".searchBtn");console.log(e.value);
-//# sourceMappingURL=index.291cdd2c.js.map
--- index.2cb74464.js
+++
@@ -1,2 +0,0 @@
-!function(){document.getElementById("main"),document.querySelector("form");var e=document.getElementById("search");document.querySelector(".searchBtn");console.log(e.value)}();
-//# sourceMappingURL=index.2cb74464.js.map
